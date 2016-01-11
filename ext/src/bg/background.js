f899928d007170e6c509cd798a9c214fa01481a1
Mahesh Murag <maheshmmurag@gmail.com>
2016-01-10 23:36:44 -0800

    added alarm to check if logged in every 5 minutes

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -4,24 +4,46 @@
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
 
-var counter = 0;
-chrome.browserAction.onClicked.addListener(function (tab) {
-    $.get("https://montavista.schoolloop.com/portal/student_home", function(data) {
+//var counter = 0;
+//chrome.browserAction.onClicked.addListener(function (tab) {
+//    $.get("https://montavista.schoolloop.com/portal/student_home", function(data) {
+//        // load response text into a new page element
+//        var SLPage = document.createElement("html");
+//        SLPage.innerHTML = data;
+//        var page = $(SLPage);
+//        var schoolName = $("#page_title_login",page);
+//        console.log($("#page_title_login",page))
+//        if(schoolName.length){//not logged in
+//            console.log("Grade update notifications won't work unless you're logged in!")
+//        }
+//        else{//logged in
+//            console.log("Logged in!")
+//            console.log($("span.page_title", page).text())
+//        }
+//    });
+//});
+
+chrome.alarms.onAlarm.addListener(function( alarm ) {
+  if(alarm.name === "NotificationsAlarm"){
+      $.get("https://montavista.schoolloop.com/portal/student_home", function(data) {
         // load response text into a new page element
         var SLPage = document.createElement("html");
         SLPage.innerHTML = data;
         var page = $(SLPage);
         var schoolName = $("#page_title_login",page);
-        console.log($("#page_title_login",page))
+//        console.log($("#page_title_login",page))
         if(schoolName.length){//not logged in
             console.log("Grade update notifications won't work unless you're logged in!")
         }
         else{//logged in
             console.log("Logged in!")
-            console.log($("span.page_title", page).text())
+//            console.log($("span.page_title", page).text())
         }
     });
+  }
 });
+
+chrome.alarms.create("NotificationsAlarm", {delayInMinutes:0, periodInMinutes:5})
 
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
