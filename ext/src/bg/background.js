e7d31de2b6c81cbb5503c2a323156460a21906f5
Mahesh Murag <maheshmmurag@gmail.com>
2016-01-15 09:53:39 -0800

    cleaned

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -1,27 +1,3 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-//var counter = 0;
-//chrome.browserAction.onClicked.addListener(function (tab) {
-//    $.get("https://montavista.schoolloop.com/portal/student_home", function(data) {
-//        // load response text into a new page element
-//        var SLPage = document.createElement("html");
-//        SLPage.innerHTML = data;
-//        var page = $(SLPage);
-//        var schoolName = $("#page_title_login",page);
-//        console.log($("#page_title_login",page))
-//        if(schoolName.length){//not logged in
-//            console.log("Grade update notifications won't work unless you're logged in!")
-//        }
-//        else{//logged in
-//            console.log("Logged in!")
-//            console.log($("span.page_title", page).text())
-//        }
-//    });
-//});
 chrome.browserAction.onClicked.addListener(function (tab) {
     checkFunc();
 });
